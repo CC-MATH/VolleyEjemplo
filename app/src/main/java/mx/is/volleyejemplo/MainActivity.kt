1614b27a1a5537ce0ef6302cbf55f78b36e27c5c
Gely <mangelica.torresh@gmail.com>
2021-04-15 13:18:16 -0500

    cambia a lint para build -stage

--- app/src/main/java/mx/is/volleyejemplo/MainActivity.kt
+++ app/src/main/java/mx/is/volleyejemplo/MainActivity.kt
@@ -5,23 +5,31 @@ import WeatherByCity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import com.android.volley.Response
 import mx.uv.tftstatistics.VolleySingleton
+
+lateinit var txtBuscar : TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        txtBuscar = findViewById(R.id.txt_search)
 
-        BuscarClima();
+        val btnBuscar: Button = findViewById(R.id.btn_search)
+        btnBuscar.setOnClickListener{
+            BuscarClima()
+        }
+
     }
 
 
     fun BuscarClima() {
         val url =
-            "http://api.openweathermap.org/data/2.5/weather?q=Veracruz&appid=156d7086924563e0b3958c649f7f0393"
+            "https://api.openweathermap.org/data/2.5/weather?q=" + txtBuscar.text +"&appid=c92933a9eecc1ffb5eef066d4182ee90"
         val mapHeaders: MutableMap<String, String> =
             mutableMapOf<String, String>() //agregar headers necesarios
         val myGsonRequest: GsonRequest<WeatherByCity> = GsonRequest<WeatherByCity>(
@@ -36,7 +44,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun myRequestSuccessListener(): Response.Listener<WeatherByCity> {
         return Response.Listener<WeatherByCity> {response ->
-            val txt_clima = findViewById<TextView>(R.id.txtClima)
+            val txt_clima = findViewById<TextView>(R.id.txt_clima)
             txt_clima.text = response.weather.get(0).description
         }
     }
